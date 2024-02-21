5ab8a1acfd47b93462240f0b36881521fa1b2815
IsabelaMattos <isabela.paula2@estudante.ifms.edu.br>
2024-02-21 16:03:24 -0400

    iniciando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 export default function App() {
 
   const nome = "João Santos";
+  const name = "Isabela Mattos";
   
   return(
     <div>
         <h1>Hello World</h1>
         <p>Desenvolvido por: {nome}</p>
+        <p>Desenvolvido por: {name}</p>
     </div>  
   );
 
 
 }
+
