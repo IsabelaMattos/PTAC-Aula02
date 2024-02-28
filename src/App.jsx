67625e3008cdbaad16a34fc302e4af371c3adbe6
IsabelaMattos <isabela.paula2@estudante.ifms.edu.br>
2024-02-28 16:07:23 -0400

    criei a pasta components e o arquivo header, e adc as tags...

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
+import Header from "./components/Header";
 export default function App() {
 
-  const nome = "João Santos";
-  const name = "Isabela Mattos";
   
   return(
-    <div>
-        <h1>Hello World</h1>
-        <p>Desenvolvido por: {nome}</p>
-        <p>Desenvolvido por: {name}</p>
-    </div>  
+    <main>
+      <Header/>
+    </main>
   );
 
 
