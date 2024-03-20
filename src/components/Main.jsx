7221b236ae2e00543d81392c2277600b1374e65f
IsabelaMattos <isabela.paula2@estudante.ifms.edu.br>
2024-03-20 16:02:49 -0400

    adicionou as tags no Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,13 @@
-export default function Main(){
+import { useState } from "react";
+
+export default function Main() {
+    const [ nome, setNome ] = useState ("")
     
+    return (
+        <main>
+            <input type="text" nome="nome-contato" id="nome" value={nome} onChange={ (event) => setNome (event.target.value)
+            }/>
+    
+        </main>
+    );
 }
