628a9639d76d4bf7819775d2ce444db89f47d8f3
IsabelaMattos <isabela.paula2@estudante.ifms.edu.br>
2024-03-27 16:01:33 -0400

    adc os inputs, as const, label, form, button...

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 
-import "./globals.css";
-
 export default function App() {
 
   
   return(
     <main>
       <Header/>
-      <Footer/>
       <Main/>
+      <Footer/>
     </main>
   );
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,53 @@
 import { useState } from "react";
 
 export default function Main() {
-    const [ nome, setNome ] = useState ("")
+    const [ nome, setNome ] = useState ("");
+    const [ telefone, setTelefone ] = useState ("");
+    const [ listaContatos, setContatos ] = useState ("");
+
+    const registrar = (event) => {
+        event.preventDefault();
+        alert("Deu certo!");
+        setContatos([...listaContatos,
+        {
+         nomeSalvo: nome,
+         telefonesalvo: telefone
+        }
+    ]);
+    }
+    console.table(listaContatos);
     
     return (
         <main>
-            <input type="text" nome="nome-contato" id="nome" value={nome} onChange={ (event) => setNome (event.target.value)
+            <center>
+            <form>
+            <label htmlFor="nome">Nome:</label>
+            <input type="text" 
+            nome="nome-contato" 
+            id="nome" value={nome} 
+            onChange={ 
+                (event) => setNome (event.target.value)
             }/>
-    
+<br/><br/>
+            <label htmlFor="telefone">Telefone:</label>
+            <input type="tel"
+            name=""
+            id="telefone" value={telefone}
+            onChange={
+                (event) => setTelefone (event.target.value)
+            }/> 
+<br/><br/>
+            <label htmlFor="listaContatos">Lista de contatos:</label>
+            <input type="list"
+            name=""
+            id="listaContatos" value={listaContatos}
+            onChange={
+                (event) => setContatos (event.target.value)
+            }/>
+<br/><br/>
+            <button>Salvar</button>
+            </form>
+            </center>
         </main>
     );
 }
